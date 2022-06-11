fb7abc6537357a41ce74b160a0f5fb7ecff886db
Kndrzej <kozinskiandrzej4@gmail.com>
2022-06-11 13:58:02 +0200

    added images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import CardBox from './components/CardBox';
 
 function App() {
   return (
     <div className="App">
+      <CardBox/>
     </div>
   );
 }

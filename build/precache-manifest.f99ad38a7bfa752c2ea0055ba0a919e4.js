cd10714981df8206b2d65f56167708316a66333d
Kndrzej <kozinskiandrzej4@gmail.com>
2022-06-11 23:52:50 +0200

    Firebase 24

---
+++ build/precache-manifest.f99ad38a7bfa752c2ea0055ba0a919e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "62095e9566ec455bfdaa",
+    "url": "/static/css/main.499b8833.chunk.css"
+  },
+  {
+    "revision": "62095e9566ec455bfdaa",
+    "url": "/static/js/main.62095e95.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "835e3fcd5d6e0010fac6",
+    "url": "/static/js/2.835e3fcd.chunk.js"
+  },
+  {
+    "revision": "ae42267ff6069bcb7427",
+    "url": "/static/js/runtime~main.ae42267f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "cce38e61d8d0b9132394231116762118",
+    "url": "/index.html"
+  }
+];
